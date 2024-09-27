5047ccca29263136a2a019dbce69a270348fc53d
Ndeen17 <nurudeeny17@gmail.com>
2024-09-27 10:50:55 +0100

    new update

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   userName: { type: String, required: true, unique: true },
   ctsBalance: { type: Number, default: 0 },
   lastFarmingTime: { type: Date }, // New field to track last farming time
+  chatId: { type: String, required: true, unique: true } // New field for chat ID
 });
 
 const User = mongoose.model('User', userSchema);
