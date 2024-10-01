ea3d8458bc8ed1a4a2826bbc12bb57fbecd87045
Ndeen17 <nurudeeny17@gmail.com>
2024-10-01 16:08:10 +0100

    bbb

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allows requests from any origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+}));
 app.use(express.json());
 
 // MongoDB connection
